2640dcd84253ca42e018ef4d0f3041434243c1d6
Theo <theo.despoudis@teckro.com>
2018-08-21 16:57:52 +0100

    Added hamming

---
+++ scheme/hamming/hamming.scm
@@ -0,0 +1,15 @@
+(define-module (hamming)
+  #:export (hamming-distance))
+(use-modules (srfi srfi-1))
+
+(define (hamming-distance a b)
+  (let ([difference (lambda (s1 s2)
+                    (map (lambda (a b) (if (char=? a b) 0 1))
+                         (string->list s1)
+                         (string->list s2)))])
+     (if (not (equal? (string-length a) (string-length b)))
+        (error "Strings must be the same length")
+        (apply + (difference a b)))
+    )
+  )
+   
